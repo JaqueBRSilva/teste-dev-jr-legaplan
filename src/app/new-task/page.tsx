fbc0a1eba326dcb8c638bceaba0cd5218a3d5045
Jaqueline Broccolo <j.silvabroccolo@gmail.com>
2024-09-22 11:08:27 -0300

    refactor: import de 'ModalProps' removido de '/new-task'

--- src/app/new-task/page.tsx
+++ src/app/new-task/page.tsx
@@ -7,12 +7,11 @@ import { CancelButton } from '../components/CancelButton/CancelButton'
 import { TitlePage } from '../components/TitlePage/TitlePage'
 import { DATA_LIST } from '../data/dataList'
 import pageGlobalStyles from '../page.module.scss'
-import { IModal_Props } from '../types/ModalProps'
 import { Task_PropTypes } from '../types/TasksProps'
 import styles from './page.module.scss'
 
 
-export default function NewTask({ ...modalProps }: IModal_Props) {
+export default function NewTask() {
     const router = useRouter()
 
     const [newTask, setNewTask] = useState('')
