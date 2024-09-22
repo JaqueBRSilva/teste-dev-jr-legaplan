515257b98db7bd56c660fc3f0d5fc76d7df8eece
Jaqueline Broccolo <j.silvabroccolo@gmail.com>
2024-09-21 22:31:25 -0300

    feat: 'dataList' adicionado

---
+++ src/app/data/dataList.ts
@@ -0,0 +1,25 @@
+export const DATA_LIST = [
+    {
+        id: 1,
+        taskDescription: "Lavar as mãos",
+        taskStatus: 'pending'
+    },
+
+    {
+        id: 2,
+        taskDescription: "Fazer um bolo",
+        taskStatus: 'pending'
+    },
+
+    {
+        id: 3,
+        taskDescription: "Lavar a louça",
+        taskStatus: 'pending'
+    },
+
+    {
+        id: 4,
+        taskDescription: "Levar o lixo para fora",
+        taskStatus: 'done'
+    }
+]
